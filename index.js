58a9bc2626a36658ffba933148bf81f2e39ef7a1
Elizabeth Billings <elizabethbillings93@gmail.com>
2022-04-20 18:46:33 -0500

    finished product

--- index.js
+++ index.js
@@ -2,37 +2,110 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateCards= require('./utils/generateCards');
-
-// TODO: Create an array of questions for user input
-const questions = [
+let teamArray=[];
+function addManager(){
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
   {
     type: 'input',
-    name: 'Name',
-    message: 'What is your name?',
+    message: 'What is your ID?',
+    name: 'id',
   },
-{
-  type: 'list',
-  message: 'What is your title?',
-  name: 'title',
-  choices:['Engineer','Manager','Employee','Intern']
-},
-{
-  type: 'input',
-  message: 'What is your ID?',
-  name: 'id',
-},
-{
-  type: 'input',
-  message: 'What is your email?',
-  name: 'email',
-},
-{
+  {
     type: 'input',
-    message: 'What is your Github?',
-    name: 'github',
-}];
+    message: 'What is your email?',
+    name: 'email',
+  },
+  {
+      type: 'input',
+      message: 'What is your office number?',
+      name: 'office',
+  }];
+  inquirer.prompt(questions)
+  .then(answers=> {
+    teamArray.push(answers);
+    menu();
+  })
+}
+function menu(){
+  const questions = [
+  {
+      type: 'list',
+      message: 'What do you want to do next?',
+      name: 'choice',
+      choices:['Add an Intern', 'Add an Engineer','Finish Building the Team']
+  }];
+  inquirer.prompt(questions)
+  .then(answers=> {
+    if(answers.choice==='Add an Intern') addIntern();
+    if(answers.choice==='Add an Engineer') addEngineer();
+    if(answers.choice==='Finish Building the Team') finish();
+  })
+}
+function addIntern(){
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+  {
+    type: 'input',
+    message: 'What is your ID?',
+    name: 'id',
+  },
+  {
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email',
+  },
+  {
+      type: 'input',
+      message: 'What is your school?',
+      name: 'school',
+  }];
+  inquirer.prompt(questions)
+  .then(answers=> {
+    teamArray.push(answers);
+    menu();
+  })
+}
+function addEngineer(){
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+  {
+    type: 'input',
+    message: 'What is your ID?',
+    name: 'id',
+  },
+  {
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email',
+  },
+  {
+      type: 'input',
+      message: 'What is your Github?',
+      name: 'github',
+  }];
+  inquirer.prompt(questions)
+  .then(answers=> {
+    teamArray.push(answers);
+    menu();
+  })
+}
+function finish(){
+  writeToFile("index.html",generateCards(teamArray));
+}
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName,data,(err)=>{
     if (err){
@@ -44,13 +117,7 @@ function writeToFile(fileName, data) {
 }
  
 
-// // TODO: Create a function to initialize app
-function init() {
-  inquirer.prompt(questions)
-  .then(function(res){
-  writeToFile("index.html",generateCards(res));
-});
-};
 
-// // Function call to initialize app
-init();
+  
+
+addManager();
--- utils/generateCards.js
+++ utils/generateCards.js
@@ -1,18 +1,65 @@
 
 
 // TODO: Create a function to generate markdown for README
-function generateCards(data) {
-  return `# ${data.title}
+function generateCards(teamArray) {
+  return `<!DOCTYPE html>
+  <html lang="en">
   
-  ${data.name}
-  
-  ${data.title}
-  
-  ${data.id}
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Elizabeth Billings">
+    <link rel="stylesheet" type="text/css" href="./assets/style.css" />
 
-  ${data.email}
-  ${data.github}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Generator</title>
+  </head>
+  <header>
+      <h1 class="team">My Team</h1>
+  </header>
+  <body>
+    ${teamArray.map(person =>{
+      if(person.office){
+        return `  
+        <div class='groupcards'> <div class="infocard">
+        <h2 class="cardtitle">${person.name}</h2>
+        <h4 class="jobtitle">Manager</h4>
+     <br>
+     <ul class="employeeid">ID: ${person.id}</ul>
+     <ul class="email">Email: ${person.email}</ul>
+     <ul class="office">Office Number: ${person.office}</ul>
+    </div>`
+      }
+      if(person.school){
+        return `   <div class="infocard">
+        <h2 class="cardtitle">${person.name}</h2>
+        <h4 class="jobtitle">Intern</h4>
+     <br>
+     <ul class="employeeid">ID: ${person.id}</ul>
+     <ul class="email">Email: ${person.email}</ul>
+     <ul class="office">School: ${person.school}</ul>
+    </div>`
+      }
+      if(person.github){
+        return `   <div class="infocard">
+        <h2 class="cardtitle">${person.name}</h2>
+        <h4 class="jobtitle">Engineer</h4>
+     <br>
+     <ul class="employeeid">ID: ${person.id}</ul>
+     <ul class="email">email: ${person.email}</ul>
+     <ul class="office">Github: ${person.github}</ul>
+    </div>
+    </div>`
+      }
+    }).join('')}
+  
+  
+    <script src="index.js"></script>
+  
+  </body>
+  </html>
+  
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateCards;
